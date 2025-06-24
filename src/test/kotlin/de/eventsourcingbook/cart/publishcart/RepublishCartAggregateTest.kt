d1cc70f012dddd80d1602b3761cc05fec7b6e310
Emerson Perales <peralesvillanuevaemerson@gmail.com>
2025-06-24 18:32:07 -0500

    republish cart aggregate test in green

--- src/test/kotlin/de/eventsourcingbook/cart/publishcart/RepublishCartAggregateTest.kt
+++ src/test/kotlin/de/eventsourcingbook/cart/publishcart/RepublishCartAggregateTest.kt
@@ -1,3 +1,76 @@
 package de.eventsourcingbook.cart.publishcart
 
-class RepublishCartAggregateTest {}
+import de.eventsourcingbook.cart.common.Event
+import de.eventsourcingbook.cart.common.support.RandomData
+import de.eventsourcingbook.cart.domain.CartAggregate
+import de.eventsourcingbook.cart.domain.commands.publishcart.PublishCartCommand
+import de.eventsourcingbook.cart.events.CartCreatedEvent
+import de.eventsourcingbook.cart.events.CartPublicationFailedEvent
+import de.eventsourcingbook.cart.events.CartPublishedEvent
+import de.eventsourcingbook.cart.events.CartSubmittedEvent
+import de.eventsourcingbook.cart.events.ItemAddedEvent
+import de.eventsourcingbook.cart.publishcart.internal.PublishCartCommandHandler
+import de.eventsourcingbook.cart.publishcart.internal.contract.ExternalPublishedCart
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import java.util.UUID
+import org.axonframework.test.aggregate.AggregateTestFixture
+import org.axonframework.test.aggregate.FixtureConfiguration
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.kafka.core.KafkaTemplate
+
+@ExtendWith(MockKExtension::class)
+class RepublishCartAggregateTest {
+
+  private lateinit var fixture: FixtureConfiguration<CartAggregate>
+
+  @RelaxedMockK private lateinit var kafkaTemplate: KafkaTemplate<String, ExternalPublishedCart>
+
+  @BeforeEach
+  fun setUp() {
+    fixture = AggregateTestFixture(CartAggregate::class.java)
+    fixture.registerAnnotatedCommandHandler(
+            PublishCartCommandHandler(kafkaTemplate, fixture.repository)
+    )
+  }
+
+  @Test
+  fun `RepublishCartAggregateTest`() {
+    // GIVEN
+    val events = mutableListOf<Event>()
+    val aggregateId = UUID.randomUUID()
+
+    events.add(RandomData.newInstance<CartCreatedEvent> { this.aggregateId = aggregateId })
+    events.add(
+            RandomData.newInstance<ItemAddedEvent> {
+              this.aggregateId = aggregateId
+              itemId = UUID.randomUUID()
+              productId = UUID.randomUUID()
+            }
+    )
+    events.add(RandomData.newInstance<CartSubmittedEvent> { this.aggregateId = aggregateId })
+    events.add(RandomData.newInstance<CartPublishedEvent> { this.aggregateId = aggregateId })
+
+    // WHEN
+    val command =
+            PublishCartCommand(
+                    aggregateId = aggregateId,
+                    orderedProducts = RandomData.newInstance {},
+                    totalPrice = RandomData.newInstance {}
+            )
+
+    // THEN
+    val expectedEvents = mutableListOf<Event>()
+
+    expectedEvents.add(
+            RandomData.newInstance<CartPublicationFailedEvent> { this.aggregateId = aggregateId }
+    )
+
+    fixture.given(events)
+            .`when`(command)
+            .expectSuccessfulHandlerExecution()
+            .expectEvents(*expectedEvents.toTypedArray())
+  }
+}
